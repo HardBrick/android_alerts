55d9167c88c76126347c2c19744560aabf634153
joseg <2guzmanjose@gmail.com>
2018-11-14 19:59:02 -0300

    Practica de Alert Dialog en clase: Se prueba 5 tipos de Alert Dialog 1) Normal 2) DatePickerAlert 3) TimePickerAlert 4) setMultiChoiceItems (Simple) 5) setMultiChoiceItems (Seleccionables)

---
+++ app/src/main/java/com/android/joseg/persistencia/MainActivity.java
@@ -0,0 +1,141 @@
+package com.android.joseg.persistencia;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAlertNormal;
+    Button btnAlertFecha;
+    Button btnAlertHora;
+    Button btnAlertList;
+    Button btnAlertListaCheck;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAlertNormal = (Button)findViewById(R.id.btnAlertNormal);
+        btnAlertFecha = (Button) findViewById(R.id.btnAlertFecha);
+        btnAlertHora = (Button)findViewById(R.id.btnAlertHora);
+        btnAlertList = (Button)findViewById(R.id.btnAlertList);
+        btnAlertListaCheck = (Button)findViewById(R.id.btnAlertListaCheck);
+
+
+        btnAlertNormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertOpc = new AlertDialog.Builder(view.getContext())
+                        .setTitle("Titulo de Alerta")
+                        .setIcon(R.drawable.ic_android_black_24dp)
+                        .setMessage("Mensaje de la Alerta")
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this,"Aceptar",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No",null);
+                AlertDialog alert = alertOpc.create();
+                alert.show();
+
+
+            }
+        });
+
+
+        btnAlertFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog fechaDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        Log.d("TAG_",dayOfMonth + "/" + (month+1) + "/" + year);
+                    }
+                }, 2018, 10, 14);
+                fechaDialog.show();
+
+
+            }
+        });
+
+        btnAlertHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog horaDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        Log.d("TAG_",hourOfDay + ":" + minute);
+                    }
+                },19,25,true);
+                horaDialog.show();
+            }
+        });
+
+        btnAlertList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] items = {
+                        "Uno",
+                        "Dos",
+                        "Tres"
+                };
+                AlertDialog.Builder alertOpc = new AlertDialog.Builder(view.getContext())
+                        .setTitle("Titulo de Alerta")
+                        .setIcon(R.drawable.ic_android_black_24dp)
+                        .setSingleChoiceItems(items, 1, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this,"Item " + i,Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                alertOpc.create().show();
+            }
+        });
+
+
+
+        btnAlertListaCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] items = {
+                        "Uno",
+                        "Dos",
+                        "Tres"
+                };
+                final boolean[] selItems = new boolean[3];
+
+                AlertDialog.Builder alertOpc = new AlertDialog.Builder(view.getContext())
+                        .setTitle("Titulo de Alerta")
+                        .setIcon(R.drawable.ic_android_black_24dp)
+                        .setMultiChoiceItems(items, selItems, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int item, boolean b) {
+                                selItems[item] = b;
+                            }
+                        })
+                        .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int item) {
+                                for (int i = 0 ; i<selItems.length;i++){
+                                    Log.d("TAG_","Item "  + i + " " + selItems[i]);
+                                }
+                            }
+                        })
+                        .setNegativeButton("Cancelar",null);
+                alertOpc.create().show();
+            }
+        });
+    }
+}
